9fd8e377b1c62c95ad188a81214ace584a63624c
Charles Overbeck <coverbec@ucsc.edu>
2019-10-21 15:21:42 -0700

    Add job handler replica count

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ set -e
 cd galaxy
 
 helm dependency update
-helm install --name galaxy --set service.type=NodePort --set webHandlers.replicaCount=${webHandlers_replicaCount:-1} .
+helm install --name galaxy --set service.type=NodePort --set webHandlers.replicaCount=${webHandlers_replicaCount:-1}  --set jobHandlers.replicaCount=${jobHandlers_replicaCount-1} .
 
